a3f10fb096c20e38328a515ab8cec1e4698b7de4
raffax <38769277+raffax@users.noreply.github.com>
2018-04-29 00:10:30 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const bodyParser = require('body-parser');
 const {actionssdk} = require('actions-on-google');
 
 const app = actionssdk({debug: true});
-console.log("MANU input: " + JSON.stringify(app.conversation.input);
+console.log("MANU input: " + JSON.stringify(app.conversation.input));
 if(app.conversation.data) {
     console.log("MANU conversation data: "+ JSON.stringify(app.conversation.data));
 }
