504a900d4348e02080d29a9a66beb233291ba206
raffax <38769277+raffax@users.noreply.github.com>
2018-05-06 19:14:50 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -3,11 +3,11 @@
 //  Vocal interaction powered by Google Actions
 //------------------------------------------------------------------------
 var watson = require('watson-developer-cloud');     // watson sdk
-var util=require('util');
-
 const express = require('express');
+
 const bodyParser = require('body-parser');
 const {actionssdk} = require('actions-on-google');
+console.log("Initializing google");
 const app = actionssdk({debug: true});
 console.log("initialized google")
 //-------------------------------------------------------------------------------------
@@ -31,7 +31,6 @@ var previous=[3,1,4,2,3,3,1,3,4]; // simulate rates from prevous inspections;
 var last_date='May 18, 2017';
 var nn=0;   // pointer to current checklist step (replaced by response.context.nn)
 var frase_da_pronunciare="";
-var ws_context=null;
 //---------------------------------------------
 // initialize dialog context variables
 //---------------------------------------------
@@ -49,6 +48,7 @@ var initialContext={
 //---------------------------------------
 // define assistant credentials
 //---------------------------------------
+console.log("PRIMA DI inizializzare assistant");
 var workspace_id = '197ccaba-7914-46d3-9e84-d50eda4ea223';
 var assistant = watson.conversation({
     username: 'b5da2fc4-f1cd-485a-a0da-a4d035f896a1',
@@ -57,79 +57,102 @@ var assistant = watson.conversation({
     version_date: '2018-02-16'
 });
 console.log("assistant initialized");
-var watsonPromise = util.promisify(assistant.message.bind(assistant));
 
 //---------------------------------------------------------------
 //  Begin conversation with a null text
 //---------------------------------------------------------------
 app.intent('actions.intent.MAIN', (conv) => {
-    watsonPromise({
-        workspace_id: workspace_id,
-        input: { text: "" },
-        context: initialContext})
-    .then(function(response) {
-        return new Promise(function (resolve,reject){
-            frase_da_pronunciare="";
-            for(var i = 0; i<response.output.text.length && i<3;i++) {
-                frase_da_pronunciare+=response.output.text[i]+". ";        
-            }
-            conv.data.ws_context=response.context;
-            conv.ask(frase_da_pronunciare);
-            resolve();
-        })
-    })
-    .catch(function(err) {
-        return new Promise(function (resolve,reject){
-            console.log("Error "+err);
-            conv.ask('Sorry, an error occurred. Goodbye!');
-            reject(err);    
-        })
-    })
+    console.log("PARTO...");
+    return prima_conversazione(conv);
+    console.log("ESCO......");
 });
+
+function prima_conversazione(conv) {
+    return new Promise(function (resolve,reject){
+        assistant.message({
+            workspace_id: workspace_id,
+            input: { text: "" },
+            context: initialContext},
+            function(err,response) {
+                if(err) {
+                    console.log("watson error "+err);
+                    reject(err);
+                }
+                else {
+                    conv.data.ws_context=response.context;
+                    var miafrase="";
+                    for(var i = 0; i<response.output.text.length && i<3; i++) {
+                        miafrase+=response.output.text[i]+". ";        
+                    }
+                    conv.ask(miafrase);
+                    resolve();
+                }
+            }
+        )
+    });
+}
 
 app.intent('actions.intent.TEXT', (conv, input) => { 
     console.log("intent: TEXT");
-    watsonPromise({
-        workspace_id: workspace_id,
-        input: { text: input },
-        context: conv.data.ws_context})
-    .then(function(response) {        
-        return new Promise(function(reolve,reject){
-            if(response.output.text.length>0 && response.output.text[0].indexOf('_action')>=0) {
-                var x = response.output.text[0].split(' ')[1];
-                if(x.indexOf('last_inspection_step')>=0) {
-                    frase_da_pronunciare='During last inspection this control was rated '+previous[nn];
-                }
-            }
-            else {
-                for(var i = 0; i<response.output.text.length;i++) {
-                    frase_da_pronunciare=response.output.text[i]+". ";        
-                }
-            }
-    //--------------------------------------
-    // Check if ispezione terminata
-    //--------------------------------------
-            if(response.context.ispezione_iniziata<10) {
-                if(response.context.ispezione_iniziata==1) {
-                    response.context.nn++;
-                    if(response.context.nn<checklist.length) {  // go to next control 
-    //  response.context.ispezione_iniziata=1;
-                        response.context.current_step=checklist[nn];                
-                    }
-                    else {  // finish
-                        return conv.close('Goodbye!');
-                    }
-                }
-            }
-            else { // finish
-                return conv.close('Goodbye!');
-            }
-            console.log("FRASE: "+frase_da_pronunciare);
-            conv.data.ws_context=response.context;        
-            conv.ask(frase_da_pronunciare);
-            resolve();    
-        })
-    });
+    return altre_conversazioni(conv,input);
 });
+  //----------------------------------------------------------
+  //    FUNCTION PROCESSRESPONSE
+  //    Process the conversation response and continue
+  //    until inspection ends
+  //----------------------------------------------------------
+function altre_conversazioni(conv,input) {
+    return new Promise (function (resolve,reject) {  
+        assistant.message({
+            workspace_id: workspace_id,
+            input: { text: input },
+            context: conv.data.ws_context,
+        }, function(err,response){
+            if(err) {
+                console.log("watson2 error: "+err);
+                reject(err);
+            }            
+            else {
+                if(response.output.text.length>0 && response.output.text[0].indexOf('_action')>=0) {
+                    var x = response.output.text[0].split(' ')[1];
+                    if(x.indexOf('last_inspection_step')>=0) {
+                        frase_da_pronunciare='During last inspection this control was rated '+previous[nn];
+                        console.log(frase_da_pronunciare);
+                    }
+                }
+                else {
+                    for(var i = 0; i<response.output.text.length;i++) {
+                        frase_da_pronunciare=response.output.text[i]+". ";        
+                    }
+                    console.log(frase_da_pronunciare);
+                }
+          
+            //--------------------------------------
+            // Check if ispezione terminata
+            //--------------------------------------
+                if(response.context.ispezione_iniziata<10) {
+                    if(response.context.ispezione_iniziata==1) {
+                        response.context.nn++;
+                        if(response.context.nn<checklist.length) {  // go to next control 
+        //                response.context.ispezione_iniziata=1;
+                            response.context.current_step=checklist[nn];                
+                        }
+                        else {  // finish
+                            conv.close('Goodbye!');
+                            resolve();
+                        }
+                    }
+                }
+                else { // finish
+                    conv.close('Goodbye!');
+                    resolve();
+                }
+                conv.data.ws_context=response.context;        
+                conv.ask(frase_da_pronunciare);
+                resolve();
+            }                
+        });
+    })
+}
 
-express().use(bodyParser.json(), app).listen(process.env.PORT || 3000);
+  express().use(bodyParser.json(), app).listen(process.env.PORT || 3000);
